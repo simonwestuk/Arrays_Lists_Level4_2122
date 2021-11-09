22ccc35edcc5047206eac4e20d4bd094f26d77d4
wests <wests@C-010053>
2021-11-09 15:41:43 +0000

    Add project files.

---
+++ Arrays_Lists/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arrays_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 100);
+
+                string oddEven = numbers[i] % 2 == 0 ? "Even" : "Odd";
+
+                Console.WriteLine($"{numbers[i]} is {oddEven}");
+            }            
+
+        }
+    }
+}
